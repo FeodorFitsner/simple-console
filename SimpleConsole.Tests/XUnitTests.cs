3f91822f70570874892b12b0d178eff1d628e20b
Feodor Fitsner <feodor@fitsner.com>
2014-02-19 09:16:09 -0800

    Added xUnit tests

--- SimpleConsole.Tests/XUnitTests.cs
+++ SimpleConsole.Tests/XUnitTests.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace SimpleConsole.Tests
 {
-    public class Class1
+    public class XUnitTests
     {
+        [Fact]
+        public void MyTest()
+        {
+            Assert.Equal(4, 2 + 2);
+        }
     }
 }
