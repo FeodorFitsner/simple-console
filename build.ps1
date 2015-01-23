27f43242cdce7563fea9adc7867218dbcedddbcd
Feodor Fitsner <feodor@appveyor.com>
2015-01-22 20:38:17 -0800

    Small change in branch B

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 # this is a progem that sets environment variable through build worker API
 .\SimpleConsole\bin\debug\SimpleConsole.exe
 
-# variables are immediately available in the same script
+# variables are immediately available in the same script aaa
 $env:GitVersion_Version
