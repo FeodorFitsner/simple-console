9ca473c788f6163de01f4774fd4de432e8806f4e
Feodor Fitsner <feodor@appveyor.com>
2015-01-25 19:37:54 -0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 # this is a progem that sets environment variable through build worker API
 .\SimpleConsole\bin\debug\SimpleConsole.exe
 
-# variables are immediately available in the same script aaa
+# variables are immediately available in the same script
 $env:GitVersion_Version
