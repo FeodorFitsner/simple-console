ace00972b793562446269801f57006bec16acd19
Feodor Fitsner <feodor@appveyor.com>
2016-02-04 19:31:46 -0800

    3 calls

--- test.cmd
+++ test.cmd
@@ -1,7 +1,18 @@
 SimpleConsole\bin\debug\SimpleConsole.exe -1073741819
 set LASTERROR=%ERRORLEVEL%
 IF %LASTERROR% NEQ 0 (
-  echo not equal to 0
+  IF %LASTERROR% NEQ -1073741819 EXIT /B %LASTERROR%
+)
+
+SimpleConsole\bin\debug\SimpleConsole.exe 0
+set LASTERROR=%ERRORLEVEL%
+IF %LASTERROR% NEQ 0 (
+  IF %LASTERROR% NEQ -1073741819 EXIT /B %LASTERROR%
+)
+
+SimpleConsole\bin\debug\SimpleConsole.exe 1
+set LASTERROR=%ERRORLEVEL%
+IF %LASTERROR% NEQ 0 (
   IF %LASTERROR% NEQ -1073741819 EXIT /B %LASTERROR%
 )
 
