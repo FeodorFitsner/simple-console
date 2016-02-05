87e06a642babb94f480c72eef523409fdbdfb848
Feodor Fitsner <feodor@appveyor.com>
2016-02-04 19:23:41 -0800

    IF %ERRORLEVEL% ...

--- test.cmd
+++ test.cmd
@@ -1,2 +1,2 @@
-SimpleConsole\bin\debug\SimpleConsole.exe -1073741819 || IF %ERRORLEVEL% NEQ -1073741819 EXIT /B 1
+SimpleConsole\bin\debug\SimpleConsole.exe -1073741819 || IF %ERRORLEVEL% NEQ -1073741819 ECHO "Original exit code was %ERRORLEVEL%" && EXIT /B 1
 SimpleConsole\bin\debug\SimpleConsole.exe -2 || IF %ERRORLEVEL% NEQ -1073741819 EXIT /B %ERRORLEVEL%
