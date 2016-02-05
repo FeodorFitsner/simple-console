5316e83d33e01f110646b24d1f97e4415eec893d
Feodor Fitsner <feodor@appveyor.com>
2016-02-04 19:22:31 -0800

    Update test.cmd

--- test.cmd
+++ test.cmd
@@ -1,2 +1,2 @@
-SimpleConsole\bin\debug\SimpleConsole.exe -2
-echo %ERRORLEVEL%
+SimpleConsole\bin\debug\SimpleConsole.exe -1073741819 || IF %ERRORLEVEL% NEQ -1073741819 EXIT /B 1
+SimpleConsole\bin\debug\SimpleConsole.exe -2 || IF %ERRORLEVEL% NEQ -1073741819 EXIT /B %ERRORLEVEL%
