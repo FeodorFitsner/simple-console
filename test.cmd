84b8e25fb9ffcee15e6a2e32046a6a3deaef1c52
Feodor Fitsner <feodor@appveyor.com>
2016-02-04 19:29:10 -0800

    nested ifs

--- test.cmd
+++ test.cmd
@@ -1,2 +1,6 @@
-SimpleConsole\bin\debug\SimpleConsole.exe -1073741819 || IF %ERRORLEVEL% NEQ -1073741819 ECHO "Original exit code was %ERRORLEVEL%" && EXIT /B 1
-SimpleConsole\bin\debug\SimpleConsole.exe -2 || IF %ERRORLEVEL% NEQ -1073741819 EXIT /B %ERRORLEVEL%
+SimpleConsole\bin\debug\SimpleConsole.exe -1073741819
+IF %ERRORLEVEL% NEQ 0 (
+  IF %ERRORLEVEL% NEQ -1073741819 EXIT /B %ERRORLEVEL%
+)
+
+rem SimpleConsole\bin\debug\SimpleConsole.exe -2 || IF %ERRORLEVEL% NEQ -1073741819 EXIT /B %ERRORLEVEL%
