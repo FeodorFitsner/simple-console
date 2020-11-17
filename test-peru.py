0db3f8fd5fab31e5e3d03bbf9d9bcaf9a32ebc02
Feodor Fitsner <feodor@appveyor.com>
2020-11-17 10:17:28 -0800

    force_utf8_in_ascii_mode_hack

--- test-peru.py
+++ test-peru.py
@@ -1,3 +1,9 @@
+if sys.stdout.encoding == 'ANSI_X3.4-1968':
+    sys.stdout = open(
+        sys.stdout.fileno(), mode='w', encoding='utf8', buffering=1)
+    sys.stderr = open(
+        sys.stderr.fileno(), mode='w', encoding='utf8', buffering=1)
+        
 f = open(r'kirapipenv\peru.yaml')
 r = f.read()
 print(r)
