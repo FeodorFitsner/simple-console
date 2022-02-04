3823b7bec6dd63487fdb1f16bf36b106f6d26150
Feodor Fitsner <feodor@appveyor.com>
2022-02-03 17:42:30 -0800

    Update test-download.ps1

--- test-download.ps1
+++ test-download.ps1
@@ -1,1 +1,2 @@
-Invoke-RestMethod -Method Get -Uri "https://ci.appveyor.com/api/buildjobs/pubck02xfr0syljn/artifacts/Widelands-Release-x64-~git(9838ae87cadfe299556a521479499e03d47ce9cd%40appveyor_fix).exe" -OutFile site~1.exe.exe -Headers @{ "Authorization" = "Bearer $env:APPVEYOR_TOKEN" }
+#Invoke-RestMethod -Method Get -Uri "https://ci.appveyor.com/api/buildjobs/pubck02xfr0syljn/artifacts/Widelands-Release-x64-~git(9838ae87cadfe299556a521479499e03d47ce9cd%40appveyor_fix).exe" -OutFile site~1.exe.exe -Headers @{ "Authorization" = "Bearer $env:APPVEYOR_TOKEN" }
+Invoke-RestMethod -Method Get -Uri "https://ci.appveyor.com/api/buildjobs/7hrt5nmpd5nb5d5n/artifacts/site~1.exe" -OutFile site~1.exe.exe
