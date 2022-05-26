7241e4798d998f473b6413821d4815e4cbb3275f
Feodor Fitsner <feodor@appveyor.com>
2022-05-26 15:43:55 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 # this is a progem that sets environment variable through build worker API
 #.\SimpleConsole\bin\debug\SimpleConsole.exe
 
-# variables are immediately available in the same script
+# variables are immediately available in the same script 
 #$env:GitVersion_Version
 Import-AzurePublishSettingsFile
